b882ee15f77dddf4162b37e57565d726cf8ccc66
Gabriel <gabrimf25@gmail.com>
2025-01-31 13:35:56 +0100

    Tienda online entregable created

---
+++ src/main/java/com/mendezfrancogabriel/tiendaonline/tiendaonline/TiendaOnline.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mendezfrancogabriel.tiendaonline.tiendaonline;
+
+/**
+ *
+ * @author alu15d
+ */
+public class TiendaOnline {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
