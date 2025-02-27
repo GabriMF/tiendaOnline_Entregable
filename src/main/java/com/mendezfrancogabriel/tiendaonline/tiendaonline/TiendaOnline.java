94414c14aed45554d1b55329b618518e417ed9bd
Gabriel <gabrimf25@gmail.com>
2025-02-27 13:11:27 +0100

    Formatted code

--- src/main/java/com/mendezfrancogabriel/tiendaonline/tiendaonline/TiendaOnline.java
+++ src/main/java/com/mendezfrancogabriel/tiendaonline/tiendaonline/TiendaOnline.java
@@ -69,8 +69,10 @@ public class TiendaOnline implements Serializable {
         tiendaOnline.guardarArchivosUnoaUno();
     }
     
-    /*
-    -------------------------------- Menus --------------------------------
+    /*  ____________________________________________________________________________________________________________________________________
+    
+                                    -------------------------------- Menus --------------------------------
+        ____________________________________________________________________________________________________________________________________
     */
 
    public void menuPrincipal(){
@@ -259,27 +261,12 @@ public class TiendaOnline implements Serializable {
             }
         }while (option != 0);
     }
-    
-    /*
-    -----------------------------------------------------------------------
-    */
-   
-    public void stock (int unidadesPed, String id) throws StockAgotado, StockInsuficiente{
-        
-        int n = articulos.get(id).getExistencias();
-        
-        if (n==0){
-            throw new StockAgotado("Stock agotado para el articulo "+ articulos.get(id).getDescripcion());
-            
-        }else if(n < unidadesPed){
-            throw new StockInsuficiente("No hay stock suficiente para el pedido. Solicita "
-                    + unidadesPed + " unidades de "+articulos.get(id).getDescripcion() 
-                    + " y solo se dispone de "+n+".");
-        }
-    }
 
-    /*
-    -------------------------------- Articulos --------------------------------
+
+    /*  ____________________________________________________________________________________________________________________________________
+    
+                                -------------------------------- Articulos --------------------------------
+        ____________________________________________________________________________________________________________________________________
     */
     public void modificarArticulos(){
                 
@@ -333,12 +320,18 @@ public class TiendaOnline implements Serializable {
         }
     }
     
-    /*
-    -------------------------------- Clientes --------------------------------
+    
+    /*  ____________________________________________________________________________________________________________________________________
+    
+                                -------------------------------- Clientes --------------------------------
+        ____________________________________________________________________________________________________________________________________
     */
     
-    /*
-    -------------------------------- Pedidos --------------------------------
+    
+    /*  ____________________________________________________________________________________________________________________________________
+    
+                                -------------------------------- Pedidos --------------------------------
+        ____________________________________________________________________________________________________________________________________
     */
     public void nuevoPedido() {
         //ARRAYLIST AUXILIAR PARA CREAR EL PEDIDO
@@ -492,40 +485,13 @@ public class TiendaOnline implements Serializable {
     }
 
     
-    public void cargaDatos(){
-        
-       clientes.put("80580845T",new Cliente("80580845T","ANA ","658111111","ana@gmail.com"));
-       clientes.put("36347775R",new Cliente("36347775R","LOLA","649222222","lola@gmail.com"));
-       clientes.put("63921307Y",new Cliente("63921307Y","JUAN","652333333","juan@gmail.com"));
-       clientes.put("02337565Y",new Cliente("02337565Y","EDU","634567890","edu@gmail.com"));
-              
-       articulos.put("1-11",new Articulo("1-11","RATON LOGITECH ST ",14,15));
-       articulos.put("1-22",new Articulo("1-22","TECLADO STANDARD  ",9,18));
-       articulos.put("2-11",new Articulo("2-11","HDD SEAGATE 1 TB  ",16,80));
-       articulos.put("2-22",new Articulo("2-22","SSD KINGSTOM 256GB",9,70));
-       articulos.put("2-33",new Articulo("2-33","SSD KINGSTOM 512GB",0,200));
-       articulos.put("3-22",new Articulo("3-22","EPSON PRINT XP300 ",5,80));
-       articulos.put("4-11",new Articulo("4-11","ASUS  MONITOR  22 ",5,100));
-       articulos.put("4-22",new Articulo("4-22","HP MONITOR LED 28 ",5,180));
-       articulos.put("4-33",new Articulo("4-33","SAMSUNG ODISSEY G5",12,580));
-       
-       LocalDate hoy = LocalDate.now();
-       pedidos.add(new Pedido("80580845T-001/2024",clientes.get("80580845T"),hoy.minusDays(1), new ArrayList<>
-        (List.of(new LineaPedido("1-11",3),new LineaPedido("4-22",3)))));                                                                                                                                                               
-       pedidos.add(new Pedido("80580845T-002/2024",clientes.get("80580845T"),hoy.minusDays(2), new ArrayList<>
-        (List.of(new LineaPedido("4-11",3),new LineaPedido("4-22",2),new LineaPedido("4-33",4)))));
-       pedidos.add(new Pedido("36347775R-001/2024",clientes.get("36347775R"),hoy.minusDays(3), new ArrayList<>
-        (List.of(new LineaPedido("4-22",1),new LineaPedido("2-22",3)))));
-       pedidos.add(new Pedido("36347775R-002/2024",clientes.get("36347775R"),hoy.minusDays(5), new ArrayList<>
-        (List.of(new LineaPedido("4-33",3),new LineaPedido("2-11",3)))));
-       pedidos.add(new Pedido("63921307Y-001/2024",clientes.get("63921307Y"),hoy.minusDays(4), new ArrayList<>
-        (List.of(new LineaPedido("2-11",5),new LineaPedido("2-33",3),new LineaPedido("4-33",2)))));
-    } 
+    /*  ____________________________________________________________________________________________________________________________________
     
-    /*
-        -------------------------------- Persistencia --------------------------------
+                                -------------------------------- Persistencia --------------------------------
+        ____________________________________________________________________________________________________________________________________
     */    
-    // Guarda Archivos todos juntos
+     
+    //  ----------  Guarda Archivos todos juntos ----------
     public void guardarArchivosGeneral(){
         try(ObjectOutputStream oosArticulos = new ObjectOutputStream(new FileOutputStream("articulosGeneral.dat"));
             ObjectOutputStream oosClientes = new ObjectOutputStream(new FileOutputStream("clientesGeneral.dat"));
@@ -548,7 +514,7 @@ public class TiendaOnline implements Serializable {
         
     }
     
-    // Lee Archivos todos juntos
+    //  ----------  Lee Archivos todos juntos ----------
     public void leerArchivosGeneral(){
         try(ObjectInputStream oisArticulos = new ObjectInputStream(new FileInputStream("articulosGeneral.dat"));
             ObjectInputStream oisClientes = new ObjectInputStream(new FileInputStream("clientesGeneral.dat"));
@@ -579,8 +545,7 @@ public class TiendaOnline implements Serializable {
         }
     }
     
-    //---------- Metodo de edu ----------
-    // Guarda archivos uno a uno.
+    //    ----------  Guarda archivos uno a uno ----------
     public void guardarArchivosUnoaUno() {
         try (ObjectOutputStream oosArticulos = new ObjectOutputStream(new FileOutputStream("articulosIndividual.dat"));
             ObjectOutputStream oosClientes = new ObjectOutputStream(new FileOutputStream("clientesIndividual.dat"));
@@ -603,8 +568,8 @@ public class TiendaOnline implements Serializable {
                  System.out.println(e.toString());
         } 
     } 
-    
-    //Lee archivos uno a uno.
+
+    //  ----------  Lee archivos uno a uno. ----------
     public void leerArchivosUnoaUno() {
         try (ObjectInputStream oisArticulos = new ObjectInputStream(new FileInputStream("articulosIndividual.dat"))){
             Articulo a;
@@ -647,44 +612,8 @@ public class TiendaOnline implements Serializable {
         }
        
     }   
-    
-    
-    
-    /* 
-    ---------- MÉTODOS PARA PERSISTENCIA DE CLIENTES EN UN ARCHIVO DE TEXTO .csv ----------
-    */
-    
-    public void clientesTxtBackup() {
-        try(BufferedWriter bfwClientes=new BufferedWriter(new FileWriter("clientes.csv"))){
-            for (Cliente c : clientes.values()) {
-                bfwClientes.write(c.getDni() + "," + c.getNombre() + "," + c.getTelefono() + "," + c.getEmail() + "\n");
-            }
-        }catch (FileNotFoundException e) {
-                 System.out.println(e.toString());   
-        }catch(IOException e){
-            System.out.println(e.toString());
-        }
-    }  
-    
-    public void clientesTxtLeer() {
-        // LEEMOS LOS CLIENTES DESDE EL ARCHIVO .csv A UNA COLECCION HASHMAP AUXILIAR Y LA IMPRIMIMOS
-        HashMap <String,Cliente> clientesAux = new HashMap();
-        try(Scanner scClientes=new Scanner(new File("clientes.csv"))){
-            while (scClientes.hasNextLine()){
-                String [] atributos = scClientes.nextLine().split("[,]");                                                              
-                Cliente c=new Cliente(atributos[0],atributos[1],atributos[2],atributos[3]); 
-                clientesAux.put(atributos[0], c);
-            }
-        }catch(IOException e){
-            System.out.println(e.toString());
-        }
-        clientesAux.values().forEach(System.out::println);
-    }  
-    
-    
-       /* METODO PARA HACER UN BACKUP DE LOS ARTICULOS EN 4 ARCHIVOS DIFERENTES SEGUN LA SECCION A LA QUE PERTENECEN (primera letra de su ID) */
-
-    /*
+     
+    //  ----- METODO PARA HACER UN BACKUP DE LOS ARTICULOS EN 4 ARCHIVOS DIFERENTES SEGUN LA SECCION A LA QUE PERTENECEN (primera letra de su ID) -----
     public void backupPorSeccion() {
         try (ObjectOutputStream oosPerifericos = new ObjectOutputStream(new FileOutputStream("Perifericos.dat"));
             ObjectOutputStream oosAlmacenamiento = new ObjectOutputStream(new FileOutputStream("Almacenamiento.dat"));
@@ -715,13 +644,14 @@ public class TiendaOnline implements Serializable {
         } catch (IOException e) {
                  System.out.println(e.toString());
         } 
-        */
+        
+        
         /* PARA COMPROBAR QUE FUNCIONA, VERIFICAMOS QUE SE HAN CREADO LOS 4 ARCHIVOS EN LA CARPETA
         RAÍZ DEL PROYECTO CON LA FECHA Y HORA ACTUAL - 
         ... Y PARA COMPROBAR EL CONTENIDO DE LOS ARCHIVOS LEEREMOS/IMPRIMIREMOS "AL VUELO" SÓLO 1 DE ELLOS
          CUYA SECCION SOLICITAMOS POR TECLADO
         */
-        /*        
+               
         System.out.println("Teclea la Seccion de los articulos CUYO ARCHIVO QUIERES COMPROBAR:");        
         char seccion=sc.next().charAt(0);
         String nombreArchivo=null;
@@ -751,15 +681,10 @@ public class TiendaOnline implements Serializable {
         } catch (ClassNotFoundException | IOException e) {
                 System.out.println(e.toString()); 
         } 
-    }  
-    */
-    
-    /* METODO PARA LEER DESDE EL ARCHIVO articulos.dat SÓLO LOS ARTICULOS DE UNA DETERMINADA SECCION INTRODUCIDA POR TECLADO
+    }   
 
-       LOS ARTICULOS DE LA SECCION ELEGIDA SE VAN CARGANDO EN UN ARRAYLIST AUXILIAR (articulosAux) Y SE MUESTRAN POR PANTALLA
-     */
-
-    /*
+    //  ------ METODO PARA LEER DESDE EL ARCHIVO articulos.dat SÓLO LOS ARTICULOS DE UNA DETERMINADA SECCION INTRODUCIDA POR TECLADO. -----
+    //  ------ LOS ARTICULOS DE LA SECCION ELEGIDA SE VAN CARGANDO EN UN ARRAYLIST AUXILIAR (articulosAux) Y SE MUESTRAN POR PANTALLA. -----  
     public void leerArchivosSeccion() {
         System.out.println("Teclea la Seccion de los articulos que quieres recuperar:");        
         String id=sc.next();
@@ -784,12 +709,40 @@ public class TiendaOnline implements Serializable {
         
         articulosAux.forEach(System.out::println);
     }
-    */
+    
+    //    ---------- MÉTODOS PARA PERSISTENCIA DE CLIENTES EN UN ARCHIVO DE TEXTO .csv ----------
+    public void clientesTxtBackup() {
+        try(BufferedWriter bfwClientes=new BufferedWriter(new FileWriter("clientes.csv"))){
+            for (Cliente c : clientes.values()) {
+                bfwClientes.write(c.getDni() + "," + c.getNombre() + "," + c.getTelefono() + "," + c.getEmail() + "\n");
+            }
+        }catch (FileNotFoundException e) {
+                 System.out.println(e.toString());   
+        }catch(IOException e){
+            System.out.println(e.toString());
+        }
+    }  
+    
+    public void clientesTxtLeer() {
+        // LEEMOS LOS CLIENTES DESDE EL ARCHIVO .csv A UNA COLECCION HASHMAP AUXILIAR Y LA IMPRIMIMOS
+        HashMap <String,Cliente> clientesAux = new HashMap();
+        try(Scanner scClientes=new Scanner(new File("clientes.csv"))){
+            while (scClientes.hasNextLine()){
+                String [] atributos = scClientes.nextLine().split("[,]");                                                              
+                Cliente c=new Cliente(atributos[0],atributos[1],atributos[2],atributos[3]); 
+                clientesAux.put(atributos[0], c);
+            }
+        }catch(IOException e){
+            System.out.println(e.toString());
+        }
+        clientesAux.values().forEach(System.out::println);
+    }  
     
     
+    /*  ____________________________________________________________________________________________________________________________________
     
-    /*
-    -------------------------------- Otros Auxiliares --------------------------------
+                                -------------------------------- Otros Auxiliares --------------------------------
+        ____________________________________________________________________________________________________________________________________
     */
     public int buscaIdArticulo(String idArticulo){
         int pos=-1;
@@ -802,4 +755,48 @@ public class TiendaOnline implements Serializable {
         
         return pos;       
     }
+    
+    public void stock (int unidadesPed, String id) throws StockAgotado, StockInsuficiente{
+        
+        int n = articulos.get(id).getExistencias();
+        
+        if (n==0){
+            throw new StockAgotado("Stock agotado para el articulo "+ articulos.get(id).getDescripcion());
+            
+        }else if(n < unidadesPed){
+            throw new StockInsuficiente("No hay stock suficiente para el pedido. Solicita "
+                    + unidadesPed + " unidades de "+articulos.get(id).getDescripcion() 
+                    + " y solo se dispone de "+n+".");
+        }
+    }
+    
+    public void cargaDatos(){
+        
+       clientes.put("80580845T",new Cliente("80580845T","ANA ","658111111","ana@gmail.com"));
+       clientes.put("36347775R",new Cliente("36347775R","LOLA","649222222","lola@gmail.com"));
+       clientes.put("63921307Y",new Cliente("63921307Y","JUAN","652333333","juan@gmail.com"));
+       clientes.put("02337565Y",new Cliente("02337565Y","EDU","634567890","edu@gmail.com"));
+              
+       articulos.put("1-11",new Articulo("1-11","RATON LOGITECH ST ",14,15));
+       articulos.put("1-22",new Articulo("1-22","TECLADO STANDARD  ",9,18));
+       articulos.put("2-11",new Articulo("2-11","HDD SEAGATE 1 TB  ",16,80));
+       articulos.put("2-22",new Articulo("2-22","SSD KINGSTOM 256GB",9,70));
+       articulos.put("2-33",new Articulo("2-33","SSD KINGSTOM 512GB",0,200));
+       articulos.put("3-22",new Articulo("3-22","EPSON PRINT XP300 ",5,80));
+       articulos.put("4-11",new Articulo("4-11","ASUS  MONITOR  22 ",5,100));
+       articulos.put("4-22",new Articulo("4-22","HP MONITOR LED 28 ",5,180));
+       articulos.put("4-33",new Articulo("4-33","SAMSUNG ODISSEY G5",12,580));
+       
+       LocalDate hoy = LocalDate.now();
+       pedidos.add(new Pedido("80580845T-001/2024",clientes.get("80580845T"),hoy.minusDays(1), new ArrayList<>
+        (List.of(new LineaPedido("1-11",3),new LineaPedido("4-22",3)))));                                                                                                                                                               
+       pedidos.add(new Pedido("80580845T-002/2024",clientes.get("80580845T"),hoy.minusDays(2), new ArrayList<>
+        (List.of(new LineaPedido("4-11",3),new LineaPedido("4-22",2),new LineaPedido("4-33",4)))));
+       pedidos.add(new Pedido("36347775R-001/2024",clientes.get("36347775R"),hoy.minusDays(3), new ArrayList<>
+        (List.of(new LineaPedido("4-22",1),new LineaPedido("2-22",3)))));
+       pedidos.add(new Pedido("36347775R-002/2024",clientes.get("36347775R"),hoy.minusDays(5), new ArrayList<>
+        (List.of(new LineaPedido("4-33",3),new LineaPedido("2-11",3)))));
+       pedidos.add(new Pedido("63921307Y-001/2024",clientes.get("63921307Y"),hoy.minusDays(4), new ArrayList<>
+        (List.of(new LineaPedido("2-11",5),new LineaPedido("2-33",3),new LineaPedido("4-33",2)))));
+    } 
 }
